7b9b8013b00c886a9e8f04b82122072d9cff8aa8
luis281197 <lchavezmelendez@gmail.com>
2019-06-20 00:37:16 -0500

    Subiendo prueba1

--- src/repositorio/Repositorio.java
+++ src/repositorio/Repositorio.java
@@ -15,6 +15,8 @@ public class Repositorio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");    }
+        System.out.println("hola");  
+        System.out.println("bye");
+    }
     
 }
